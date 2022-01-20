1d556a0a0ac4eb5e9f8afd189937438c8de310e2
Muhammad Fadhil Adha <fadhil.sonic@gmail.com>
2022-01-20 16:44:28 +0700

    I'm so confuse

---
+++ database/migrations/2022_01_20_093759_pelanggan.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Pelanggan extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pelanggan', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama')->nullable();
+            $table->string('subdomain')->unique();
+            $table->string('custom_domain')->unique()->nullable();
+            $table->string('tema')->nullable();
+            $table->string('logo')->nullable();
+            $table->string('favicon')->nullable();
+            $table->enum('jenis',['seller','distributor'])->default('distributor');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExist('pelanggan');
+    }
+}
